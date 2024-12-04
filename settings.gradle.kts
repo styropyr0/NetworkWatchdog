583bd5ed79880523bb876992a52aa0d29b9424d9
saurav <sauravsajeev202@gmail.com>
2024-12-04 22:14:46 +0530

    Gradle publishing configurations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,10 +12,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
