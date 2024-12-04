d99852cc0169be00533ee55935968af1b613e3ec
saurav <sauravsajeev202@gmail.com>
2024-12-04 21:37:16 +0530

    Added more accessible network parameters

--- network_watchdog/build.gradle.kts
+++ network_watchdog/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
